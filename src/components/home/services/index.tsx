9ba7b918c952c25a93b44fe674d579f3a0ca6cbc
deverick <dncellassistec@gmail.com>
2025-07-13 17:15:09 -0300

    criado componente services

---
+++ src/components/home/services/index.tsx
@@ -0,0 +1,46 @@
+import { HomeProps } from "@/utils/home.type";
+import styles from "./styles.module.scss";
+import Image from "next/image";
+
+export function Services({ object }: HomeProps) {
+  return (
+    <>
+      <section className={styles.containerAbout}>
+        <article className={styles.inerContainer}>
+          <h1 className={styles.title}>Sobre</h1>
+          <p>{object.metadata.about.description}</p>
+        </article>
+        <div className={styles.bannerAbout}>
+          <Image
+            className={styles.imageAbout}
+            alt="Imagerm iluestrativa sobre a empresa"
+            quality={100}
+            fill={true}
+            src={object.metadata.about.banner.url}
+          />
+        </div>
+      </section>
+
+      <h2 className={styles.servicesTitle}>Conheça nossos serviços</h2>
+
+      <section className={styles.services}>
+
+          {object.metadata.services.map((service) => (
+            <article key={service.description} className={styles.service}>
+              <div className={styles.inerService}>
+                <Image
+                  className={styles.imageService}
+                  alt="Tmagem do serviço"
+                  quality={100}
+                  fill={true}
+                  src={service.image.url}
+                />
+              </div>
+              <p>{service.description}</p>
+            </article>
+          ))}
+
+      </section>
+    </>
+  );
+}
