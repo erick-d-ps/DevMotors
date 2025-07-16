82a14f02547f9a2470194ac99c1cd15284b48216
deverick <dncellassistec@gmail.com>
2025-07-15 22:39:51 -0300

    fetch da api paginas

--- src/utils/actions/get-data.ts
+++ src/utils/actions/get-data.ts
@@ -1,7 +1,7 @@
 export async function getDataHome() {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/objects/686d8f0dd5e8b76ec441a7fb?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`
+      `${process.env.NEXT_PUBLIC_API_URL}/objects/686d8f0dd5e8b76ec441a7fb?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`, { next: { revalidate: 120 } }
     );
 
     if (!res.ok) {
@@ -13,5 +13,23 @@ export async function getDataHome() {
     
   } catch (err) {
     throw new Error("Failed to fetch data");
+  }
+}
+
+
+export async function getSubMenu(){
+  try{
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B%22type%22:%22pages%22%7D&limit=10&skip=0&read_key=${process.env.READ_KEY}&depth=1&props=slug,title`, { next: { revalidate: 120 } }
+    );
+   
+    if(!res.ok) {
+      throw new Error("failed to fetch menu data")
+    }
+
+    return res.json();
+
+  }catch(err){
+    throw new Error("failed to fetch menu data")
   }
 }
