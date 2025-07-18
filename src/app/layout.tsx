a7e8a7edbe7941f10c791330c837a0280384fae9
deverick <dncellassistec@gmail.com>
2025-07-18 19:31:48 -0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,32 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import "./globals.scss";
-import { Header } from '@/components/header'
+import { Header } from "@/components/header";
 
 const inter = Inter({
-  subsets: ['latin'], 
-  display: 'swap',  
-  variable: '--font-inter', 
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
   title: "DevMotors - sua oficina especializada!",
   description: "Oficina de carros em Paraníba MS",
+  keywords: ["oficina", "oficina de carros", "carros", "manutenção de carros"],
+  openGraph: {
+    title: "DevMotors - sua oficina especializada!",
+    images: [`${process.env.NEXT_PUBLIC_URL}/opengraph.png`],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -22,11 +37,11 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={inter.variable}>
       <body>
-        <Header/>
+        <Header />
         {children}
-        <p style={{ textAlign: "center", marginTop: 54, marginBottom: 14}}>
-        Todos diritos reservados DevMotors @{`${new Date().getFullYear()}`}
-      </p>
+        <p style={{ textAlign: "center", marginTop: 54, marginBottom: 14 }}>
+          Todos diritos reservados DevMotors @{`${new Date().getFullYear()}`}
+        </p>
       </body>
     </html>
   );
