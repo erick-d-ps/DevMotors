67f82b563803ef0cf9146fb84f0057db9760be02
deverick <dncellassistec@gmail.com>
2025-07-22 22:02:57 -0300

    aplicado Suspense para fallback do componente loadingPost

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,12 +1,9 @@
-import styles from "./styles.module.scss";
+import { Suspense } from "react";
 import { getItemBySlug } from "@/utils/actions/get-data";
 import { PostProps } from "@/utils/post.type";
-import { Hero } from "@/components/hero";
-import { Phone } from "lucide-react";
-import { Container } from "@/components/container";
-import Image from "next/image";
 import { Metadata } from "next";
-import { title } from "process";
+import { Content } from "./components";
+import { LoadingPost } from "./components/loading"
 
 export async function generateMetadata({
   params: paramsPromise,
@@ -63,47 +60,11 @@ export default async function Page({
   const params = await paramsPromise;
   const { slug } = params;
 
-  const { objects }: PostProps = await getItemBySlug(slug);
-
   return (
     <>
-      <Hero
-        heading={objects[0].title}
-        buttonTitle={objects[0].metadata.button.title}
-        bannerUrl={objects[0].metadata.banner.url}
-        buttonUrl={objects[0].metadata.button.url}
-        icon={<Phone size={24} color="#fff" />}
-      />
-      <Container>
-        <section className={styles.about}>
-          <article className={styles.inerAbout}>
-            <h1 className={styles.title}>
-              {objects[0].metadata.description.title}
-            </h1>
-            <p>{objects[0].metadata.description.text}</p>
-
-            {objects[0].metadata.description.button_active && (
-              <a
-                href={objects[0].metadata.description.button_url as string}
-                target="_blank"
-                className={styles.link}
-              >
-                {objects[0].metadata.description.button_title}
-              </a>
-            )}
-          </article>
-          <div className={styles.bannerAbout}>
-            <Image
-              className={styles.imageAbout}
-              alt={objects[0].metadata.description.title}
-              quality={100}
-              fill={true}
-              sizes="100%"
-              src={objects[0].metadata.description.banner.url}
-            />
-          </div>
-        </section>
-      </Container>
+      <Suspense fallback={ <LoadingPost/> }>
+        <Content slug={slug} />
+      </Suspense>
     </>
   );
 }
